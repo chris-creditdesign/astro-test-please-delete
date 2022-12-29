9278fcf2d3cdcdebab9c7060b911c73bf168cee2
Chris Ryan <creditdesign@gmail.com>
2022-12-29 11:58:56 +0000

    Adds files from Astro tutorial

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+  export let messages = ["test"];
+
+  let mounted = false;
+  let randomMessage = messages[0];
+
+  const getRandomMessage = () => {
+    randomMessage = messages[Math.floor(Math.random() * messages.length)];
+  };
+
+  onMount(() => {
+    mounted = true;
+  });
+</script>
+
+<h3>Hello {randomMessage}!</h3>
+
+{#if mounted}
+  <button on:click={() => getRandomMessage()}>Change message</button>
+{/if}
